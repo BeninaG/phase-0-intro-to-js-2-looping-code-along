9db9a0442138b7d476b7b4ba08dcc331a88db1bf
nina <nina.gravatt@gmail.com>
2022-04-18 15:58:30 -0500

    commit tree 911af9e585d79e397c6806fd51f64051a46722da

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messwriteCards = [];
+  for (let i = 0; i < names.length; i++) {
+    messwriteCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messwriteCards;
+}
+
+
+function countDown() {
+    let down = 10;
+    while (down >= 0) {
+        console.log(--down);
+        
+        
+    
+
+    }
+
+    
+}
